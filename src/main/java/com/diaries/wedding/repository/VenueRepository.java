a53bf96171b8208266b906055fb89d67f3bf0b6b
rajiv <rajiv@kId>
2017-05-21 17:37:52 +0530

    Venue Rent added in View Venue page

--- src/main/java/com/diaries/wedding/repository/VenueRepository.java
+++ src/main/java/com/diaries/wedding/repository/VenueRepository.java
@@ -20,6 +20,6 @@ public interface VenueRepository extends MongoRepository<Venue,String>{
 	Venue findById(String id);
 //@Query(value="{ 'firstname' : ?0 }", fields="{ 'firstname' : 1, 'lastname' : 1}")
 //List<Halls> findByHalls_HallName(String hallName);
-	@Query(value="{} ", fields="{ 'name' : 1, 'state' : 1,'address':1,'shortDescr':1,'otherHighlights':1,'vegPrice':1,'nonVegPrice':1,'city':1,'area':1}")
+	@Query(value="{} ", fields="{ 'name' : 1, 'state' : 1,'address':1,'shortDescr':1,'otherHighlights':1,'vegPrice':1,'nonVegPrice':1,'city':1,'area':1,'venueRent':1}")
 	List<Venue> findAll();
 }
