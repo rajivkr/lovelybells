9f9d06c7e52b72f7e2c0af61e58f4cb1251233c6
Alexander <user@user-PC>
2017-06-09 18:32:27 +0530

    Venue Details Page image loading issue fixed

--- src/main/resources/static/lovelybells/scripts/Venue/venueDetailsController.js
+++ src/main/resources/static/lovelybells/scripts/Venue/venueDetailsController.js
@@ -1,36 +1,33 @@
 /**
  * 
  */
-app.controller('VenueDetailsController', ['$scope', 'VenueService','$location', function ($scope, VenueService,$location) {
+app.controller('VenueDetailsController', ['$scope', 'VenueService','$location','$timeout', function ($scope, VenueService,$location,$timeout) {
 	
 
-	VenueService.getFullVenue(VenueService.getVenueId()).then(function (data) {
+	
+	$scope.intializePage = function(){
+    	
+		console.log('testme');
+		 $timeout(function(){
+			 jQuery('#camera_wrap_1').camera({
+		  			thumbnails: true
+		  	      });
+		      	
+		  		jQuery('#camera_wrap_2').camera({
+		  			height: '400px',
+		  			loader: 'bar',
+		  			pagination: false,
+		  			thumbnails: true
+		  		});
+			 
+		 }, 500);
+   	 
+		
+    
+    };
+    VenueService.getFullVenue(VenueService.getVenueId()).then(function (data) {
         $scope.venue= data;
-        $scope.intializePage = function(){
-        	
-        	$(document).ready(function() {
-        		 tabModule.init();
-                $('#example1').gentleSelect({ 
-                    columns: 3,
-                    itemWidth: 100,
-                    title: "Select a fruit",
-        	    hideOnMouseOut: true
-                });
-        		 $('#example3').gentleSelect({ 
-                    columns: 3,
-                    itemWidth: 100
-                });
-        		 
-              });
-        
-        
-        	
-        
-        };
-        
-      
-        
-    })
+    });
 	   
 	 
 }])
