e4d3db2e4b728b0cf6bacd755567ea3e389ca1e4
Nandhu <Nandhu@Nandhu-PC>
2017-06-10 18:18:49 +0530

    Venue details image loader issue changes

--- src/main/resources/static/lovelybells/scripts/Venue/venueDetailsController.js
+++ src/main/resources/static/lovelybells/scripts/Venue/venueDetailsController.js
@@ -1,33 +1,21 @@
 /**
  * 
  */
-app.controller('VenueDetailsController', ['$scope', 'VenueService','$location','$timeout', function ($scope, VenueService,$location,$timeout) {
-	
+app.controller('VenueDetailsController', ['$scope', 'VenueService','$location', function ($scope, VenueService,$location) {
 
-	
-	$scope.intializePage = function(){
-    	
-		console.log('testme');
-		 $timeout(function(){
-			 jQuery('#camera_wrap_1').camera({
-		  			thumbnails: true
-		  	      });
-		      	
-		  		jQuery('#camera_wrap_2').camera({
-		  			height: '400px',
-		  			loader: 'bar',
-		  			pagination: false,
-		  			thumbnails: true
-		  		});
-			 
-		 }, 500);
-   	 
-		
-    
-    };
-    VenueService.getFullVenue(VenueService.getVenueId()).then(function (data) {
+
+	$scope.venue1="nanda";
+	VenueService.getFullVenue(VenueService.getVenueId()).then(function (data) {
         $scope.venue= data;
+       
+        
+      console.log($scope.venue);
+        
     });
+	
+	
+	
+	
 	   
 	 
 }])
