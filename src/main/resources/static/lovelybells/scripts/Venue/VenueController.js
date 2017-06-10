b4eaa048da47a972fc1ed45011a67e0375b33cca
Nandhu <Nandhu@Nandhu-PC>
2017-06-11 01:00:24 +0530

    Added venue comparator

--- src/main/resources/static/lovelybells/scripts/Venue/VenueController.js
+++ src/main/resources/static/lovelybells/scripts/Venue/VenueController.js
@@ -1,4 +1,4 @@
-app.controller('VenueController', ['$scope', 'VenueService','$location', function ($scope, VenueService,$location) {
+app.controller('VenueController', ['$scope', 'VenueService','$location',function ($scope, VenueService,$location) {
 	
 	$scope.venueList=[]; 
 	$scope.order="-added";
@@ -22,6 +22,28 @@ app.controller('VenueController', ['$scope', 'VenueService','$location', functio
 		VenueService.setVenueId(venueId);
 		$location.path('/VenueDetails');
 		};
+		
+		$scope.venueCompare=[];
+		$scope.checkValue=function(venue,val)
+		{
+			if(!val)
+			{
+				
+				var index=$scope.venueCompare.indexOf(venue);
+				
+				$scope.venueCompare.splice(index,1);
+			}
+			else
+			{
+			$scope.venueCompare.push(venue);
+			
+			}
+			
+			console.log($scope.venueCompare+" "+val)
+		}
 	   
-	 
+		  
+		
 }])
+ 
+
