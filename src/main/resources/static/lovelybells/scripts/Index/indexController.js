e169d698bb0ed4fa069a562455d3f85287d86135
user <user@user-PC>
2017-06-14 23:12:29 +0530

    Fixed image slider issue for venues in index page

--- src/main/resources/static/lovelybells/scripts/Index/indexController.js
+++ src/main/resources/static/lovelybells/scripts/Index/indexController.js
@@ -8,19 +8,28 @@ $scope.intializePage = function(){
           itemWidth: 100
       });
 		 
-		 appMaster.smoothScroll();
-
-		    appMaster.reviewsCarousel();
-
-		    appMaster.screensCarousel();
-
-		    appMaster.animateScript();
-
-		    appMaster.revSlider();
-
-		    appMaster.scrollMenu();
-
-		    appMaster.placeHold();
+		
 }
+
+VenueService.getVenues().then(function (data) {
+    $scope.venues = data;
+})
+
+
+$scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
+        	appMaster.smoothScroll();
+
+            appMaster.reviewsCarousel();
+
+            appMaster.screensCarousel();
+
+            appMaster.animateScript();
+
+            appMaster.revSlider();
+
+            appMaster.scrollMenu();
+
+            appMaster.placeHold();
+        });
 	 
 }])
--- src/main/resources/static/lovelybells/scripts/lovelybells-app.start.js
+++ src/main/resources/static/lovelybells/scripts/lovelybells-app.start.js
@@ -28,3 +28,15 @@ app.config(['$routeProvider',
         //})
         ;
     }]);
+app.directive('onFinishRender', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            if (scope.$last === true) {
+                $timeout(function () {
+                    scope.$emit(attr.onFinishRender);
+                },500);
+            }
+        }
+    }
+})
